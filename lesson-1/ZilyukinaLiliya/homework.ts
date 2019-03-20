7ff397a7ce85d601515ca24f2434e66b7e5f14e4
Liliya Zilyukina <zilyukina.liliya@gmail.com>
2019-03-21 00:08:13 +0300

    [upd]

--- lesson-1/ZilyukinaLiliya/homework.ts
+++ lesson-1/ZilyukinaLiliya/homework.ts
@@ -1,51 +1,26 @@
-/*в задании не уточняются типы элементов, поэтому допустила в учебных примерах использование any*/
+type sn = string | number;
+type snb = sn | boolean;
+type primitives = snb | null | undefined;
 
-function isInArray(a: any[], ...args: any[]): boolean {
-  if (!args.length) return false;
-  return args.every(item => a.indexOf(item) !== -1);
+function isInArray(a: snb[], ...args: snb[]): boolean {
+  return args.every((item: snb) => a.indexOf(item) !== -1);
 }
 
-function summator(...args: (number | string)[]){
-  const strIncluded = args.some(item => typeof item === 'string');
-
-  function _sum(a: any[]): number | string {
-    let arr;
-    let initialVal;
-    if (strIncluded) {
-      arr = a.map(item => item.toString());
-      initialVal = '';
-    } else {
-      arr = a.map(item => Number(item));
-      initialVal = 0;
-    }
-    return arr.reduce((result, item) => result + item, initialVal);
-  }
-  return _sum(args);
+function summator(...args: sn[]): number {
+  return args.reduce<number>((summ: number, item: sn): number => {
+    return summ + (Number(item) || 0);
+  }, 0);
 }
 
-function getUnique(...args: any[]) {
-  let arr: any[] = [];
-  args.forEach(item => {
-    if (arr.indexOf(item) === -1) arr.push(item);
+function getUnique(...args: primitives[]): primitives[] {
+  return Array.from(new Set(args));
+}
+
+function toMatrix(data: primitives[], rowSize: number): primitives[][] {
+  const length: number = Math.ceil(data.length / rowSize);
+
+  return Array(length).fill(0).map((_, idx) => {
+    const row = idx * rowSize;
+    return data.slice(row, row + rowSize);
   });
-  return arr;
-}
-
-function toMatrix(data: any[], rowSize: number) {
-  let result: any[] = [];
-  let i: number = 1;
-  let _arr: any[] = [];
-
-  data.forEach(item => {
-    _arr.push(item);
-    if (i === rowSize) {
-      result.push(_arr);
-      i = 1;
-      _arr = [];
-    } else {
-      i++;
-    }
-  });
-  if (_arr.length) result.push(_arr);
-  return result;
 }

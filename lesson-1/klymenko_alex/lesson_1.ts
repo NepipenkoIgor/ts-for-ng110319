63b71592adce9924593bee367304d1c02142f9ef
alex_klymenko <klymenko.sasha21@gmail.com>
2019-03-21 17:27:35 +0200

    homework for TypeScript #1, change type: any

--- lesson-1/klymenko_alex/lesson_1.ts
+++ lesson-1/klymenko_alex/lesson_1.ts
@@ -1,47 +1,44 @@
-function isInArray(firstParam: any[], ...args: []): boolean {
+type ns = number | string;
+type nsb = ns | boolean;
+type arr = nsb[];
+
+function isInArray(firstParam: nsb[], ...args: []): boolean {
     for (let i = 0; i < args.length; i++) {
         if (firstParam.indexOf(args[i]) === -1) {
             return false;
         }
     }
-
     return true;
 }
 
-function summator(...args: (number | string)[]): number {
+function summator(...args: ns[]): number {
     let result = 0;
-
     args.forEach(value => {
         const val = Number(value);
         result = result + val;
     });
-
     return result;
 }
 
-function getUnique(...args: any[]): any[] {
-    const result: any[] = [];
-
-    args.forEach((value: any) => {
+function getUnique(...args: nsb[]): nsb[] {
+    const result: nsb[] = [];
+    args.forEach((value: nsb) => {
         if (result.indexOf(value) === -1) {
             result.push(value);
         }
     });
-
     return result;
 }
 
-function toMatrix(data: [], rowSize: number): any[] {
-    const result: any[] = [];
+function toMatrix(data: nsb[], rowSize: number): arr[] {
+    const result: arr[] = [];
     const max = Math.ceil(data.length / rowSize);
     let start = 0;
-    let end = rowSize;
-
+    let end: number = rowSize;
     for (let i = 0; i < max; i++) {
         result.push(data.slice(start, end));
         start = end;
         end += rowSize;
     }
-
     return result;
 }

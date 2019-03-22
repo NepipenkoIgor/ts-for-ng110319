da265650c0b026d38495e7a7e5b99fc789012fdf
MSolovyanova <solovyanovamaria@yandex.ru>
2019-03-22 15:44:46 +0300

    Fixed types

--- lesson-1/SolovyanovaMaria/homework1.ts
+++ lesson-1/SolovyanovaMaria/homework1.ts
@@ -1,14 +1,14 @@
 // 1
 type snbnus = string | number | boolean | null | undefined | symbol;
 
-const isInArray = (targetArray: snbnus[], ...rest: snbnus[]) => {
+const isInArray = (targetArray: snbnus[], ...rest: snbnus[]): boolean => {
     return rest.every(currentElement => targetArray.includes(currentElement));
 };
 
 // 2
 type sn = string | number;
 
-const summator = (...elements: sn[]) => {
+const summator = (...elements: sn[]): number => {
     return elements.reduce((acc: number, currentElement: sn) => {
         if (typeof(currentElement) === 'string') {
             currentElement = Number(currentElement);
@@ -18,12 +18,12 @@ const summator = (...elements: sn[]) => {
 };
 
 // 3
-const getUnique = (...array: snbnus[]) => {
+const getUnique = (...array: snbnus[]): snbnus[] => {
     return array.reduce((acc: snbnus[], currentElement: snbnus) =>
         (acc.includes(currentElement) ? acc : [...acc, currentElement]), []);
 };
 
 // 4
-const toMatrix = (data: snbnus[], rowSize: number) => {
+const toMatrix = (data: snbnus[], rowSize: number): snbnus[][] => {
     return data.map(currentElement => Array(rowSize).fill(currentElement));
 };

62a2d6d28a7867252f0dac4f67206bb39aea9399
MSolovyanova <msolovyanova@at-consulting.ru>
2019-03-30 12:14:24 +0300

    Added homework2 Solovyanova Maria

--- lesson-2/SolovyanovaMaria/homework2.ts
+++ lesson-2/SolovyanovaMaria/homework2.ts
@@ -1,23 +1,46 @@
 import { fromEvent, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, mergeMap } from 'rxjs/operators';
 
-
-const input: HTMLElement = document.getElementById('search') as HTMLElement;
+const input: HTMLInputElement = document.getElementById('search') as HTMLInputElement;
 const getEvent$: Observable<KeyboardEvent> = fromEvent(input, 'keypress') as Observable<KeyboardEvent>;
+const ul: HTMLUListElement = document.getElementById('searchResult') as HTMLUListElement;
+const label: HTMLLabelElement = document.getElementById('resultCount') as HTMLLabelElement;
 
-const sendRequestToGit$ = getEvent$.pipe(
-    filter((event: KeyboardEvent) => event.code === 'Enter')
-);
+interface IGitResponseInfo {
+    incomplete_results: boolean;
+    items: [];
+    total_count: number;
+}
 
-sendRequestToGit$.subscribe(filteredEvent => console.log(filteredEvent));
+interface IGitResponse {
+    name: string;
+    git_url: string;
+}
 
-const getRepos = () => {
-    return fetch('https://api.github.com/repositories')
-        .then(res => console.log(res),
-              err => console.error(err));
+const renderResponse = (response: IGitResponseInfo) => {
+    const {incomplete_results, items, total_count} = response;
+    if (!incomplete_results) {
+        label.textContent = `Всего результатов: ${total_count}`;
+        items.map((currentElement: IGitResponse) => {
+            const liElement: HTMLLIElement = document.createElement('li');
+            const href: HTMLAnchorElement = document.createElement('a');
+            href.setAttribute('href', currentElement.git_url);
+            href.text = currentElement.name;
+            liElement.append(href);
+            ul.append(liElement);
+        });
+    }
 };
 
-sendRequestToGit$.subscribe((event: KeyboardEvent) => {
-    console.log(event);
-    getRepos();
-});
+const getRepos = () => {
+    return fetch(`https://api.github.com/search/repositories?q=${(input.value).replace(' ', '+')}+in:name`)
+        .then(res => res.json(),
+            err => console.error(err));
+};
+
+const sendRequestToGit$ = getEvent$.pipe(
+    filter((event: KeyboardEvent) => event.code === 'Enter'),
+    mergeMap(_keypress => getRepos())
+);
+
+sendRequestToGit$.subscribe(result => renderResponse(result));
